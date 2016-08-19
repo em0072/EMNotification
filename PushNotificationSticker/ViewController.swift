d4671e4334ba9f67c21f13139d49bde4bf133b5b
Евгений Митько <emitko@rfc1918.private.ip.localhost>
2016-08-19 17:17:27 +0300

    Initial Commit

---
+++ PushNotificationSticker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PushNotificationSticker
+//
+//  Created by Евгений Митько on 19/08/16.
+//  Copyright © 2016 Evgeny Mitko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
